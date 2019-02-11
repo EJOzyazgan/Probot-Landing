ac2a9dc117c0bef9e82693d9faa5cdfa1729818f
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-02-11 15:46:52 -0800

    Routing

--- src/app/landing-pages/landing-pages.component.ts
+++ src/app/landing-pages/landing-pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing-pages',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    return this.router.navigate(['landing-pages/home']);
   }
 
 }
