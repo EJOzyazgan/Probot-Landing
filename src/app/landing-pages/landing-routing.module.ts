90244ceab9b8f38d919cd4b9d9019006d5fb499f
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-02-12 23:56:58 -0800

    XILO-27 Initial Event Page

--- src/app/landing-pages/landing-routing.module.ts
+++ src/app/landing-pages/landing-routing.module.ts
@@ -4,12 +4,14 @@ import {LandingPagesComponent} from './landing-pages.component';
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
+import {EventComponent} from './event/event.component';
 
 const landingPagesRoutes: Routes = [
   { path: '', component: LandingPagesComponent, children: [
       { path: 'home', component: HomeComponent},
       { path: 'about', component: AboutComponent},
       { path: 'contact', component: ContactComponent},
+      { path: 'events', component: EventComponent}
     ]}
 ];
 
