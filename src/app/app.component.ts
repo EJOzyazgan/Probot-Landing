9fa39b6d82f19fe3e67b872b762e10165d473e6d
EJ Ozyazgan <ejozyazgan@gmail.com>
2019-03-07 12:16:16 -0800

    Https redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
-  title = 'Probot-landing';
+export class AppComponent implements OnInit {
+  location: Location;
+
+  ngOnInit() {
+
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+      }
+    }
+  }
 }
